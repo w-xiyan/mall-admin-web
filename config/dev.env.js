39acf0b5281d6f41e73b63ebb03c295945339602
w-xiyan <35488950+w-xiyan@users.noreply.github.com>
2022-05-31 10:48:35 +0800

    微服务地址修改

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,7 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"http://localhost:8080"'
+  //BASE_API: '"http://localhost:8201"'
+  BASE_API: '"http://localhost:8201/mall-admin"'
+  
 })
